0a16f6ebfd28b1a369f23a71ccaef268451c7b1e
Josh <quist>
2021-09-23 11:04:08 -0500

    Disemvowel progress
    Disemvowel progress; haven't fixed memory leaks yet.

--- disemvowel/disemvowel.c
+++ disemvowel/disemvowel.c
@@ -1,7 +1,41 @@
 #include <stdlib.h>
+#include <string.h>
+#include <stdlib.h>
 
 #include "disemvowel.h"
 
 char *disemvowel(char *str) {
-  return (char*) "";
+  int i = 0;
+  int counter = 0;
+  int counter2 =0;
+  int size;
+  size = strlen(str);
+  char *result;
+  for(i=0; i<size; i++){
+    if(str[i]=='a'){
+      str[i] = 0;
+    }
+    else if(str[i]=='e'){
+      str[i] = 0;
+    }
+    else if(str[i]=='i'){
+      str[i] = 0;
+    }
+    else if(str[i]=='o'){
+      str[i] = 0;
+    }
+    else if(str[i]=='u'){
+      str[i] = 0;
+    }
+    else counter++;
+  }
+  result = (char*)calloc(counter+1, sizeof(char));
+  for (i=0; i<size; i++){
+    if (str[i]!=0){
+      result[counter2]=str[i];
+      counter2++;
+    }
+  }
+  result[counter2]='\0';
+  return result;
 }
--- disemvowel/main.c
+++ disemvowel/main.c
@@ -6,11 +6,14 @@
 int main(int argc, char *argv[]) {
   char *line;
   size_t size;
+  char *result;
   
   size = 100;
   line = (char*) malloc (size + 1);
 
+
   while (getline(&line, &size, stdin) > 0) {
-    printf("%s\n", disemvowel(line));
+    result = disemvowel(line);
+    printf("%s\n", result);
   }
 }
